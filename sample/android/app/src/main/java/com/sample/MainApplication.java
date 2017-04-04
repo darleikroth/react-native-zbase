1c4d6a834d122a471193758f4eda0422846cd37e
Darlei Kroth <maildarlei@icloud.com>
2017-04-04 17:20:12 -0300

    Removed android library react-native-orientation

--- sample/android/app/src/main/java/com/sample/MainApplication.java
+++ sample/android/app/src/main/java/com/sample/MainApplication.java
@@ -3,7 +3,6 @@ package com.sample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.github.yamill.orientation.OrientationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,7 +24,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new OrientationPackage(),
             new VectorIconsPackage()
       );
     }
