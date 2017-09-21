878945e168d4225165832c9c7a9573911c61c560
Darlei Kroth <maildarlei@icloud.com>
2017-09-21 15:55:38 -0300

    Migrate TouchableView to React Component

--- TouchableView/index.js
+++ TouchableView/index.js
@@ -1,32 +1,43 @@
 import React from 'react';
+import PropsType from 'props-type';
 import {
   TouchableNativeFeedback,
   TouchableOpacity,
   Platform,
 } from 'react-native';
 
-const TouchableView = props =>
+class TouchableView extends React.Component
 {
-  if (Platform.OS === 'ios') {
+  render()
+  {
+    const props = this.props;
+
+    if (Platform.OS === 'ios') {
+      return (
+        <TouchableOpacity
+          {...props}
+          onPress={props.onPress}
+        >
+          {props.children}
+        </TouchableOpacity>
+      );
+    }
+  
     return (
-      <TouchableOpacity
+      <TouchableNativeFeedback
         {...props}
+        background={TouchableNativeFeedback.Ripple(props.background || '#E0E0E0', true)}
         onPress={props.onPress}
       >
         {props.children}
-      </TouchableOpacity>
+      </TouchableNativeFeedback>
     );
   }
-
-  return (
-    <TouchableNativeFeedback
-      {...props}
-      background={TouchableNativeFeedback.Ripple(props.background || '#E0E0E0', true)}
-      onPress={props.onPress}
-    >
-      {props.children}
-    </TouchableNativeFeedback>
-  );
 }
+
+TouchableView.propsType = {
+  onPress: PropsType.func,
+  background: PropsType.string,
+};
 
 module.exports = TouchableView;
