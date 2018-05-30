a1d6c967508efc335a5a64269266a2bd41fa7a67
Darlei Kroth <maildarlei@icloud.com>
2018-05-30 18:07:16 -0300

    remove flag required prop statusBarColor

--- SelectItems/index.js
+++ SelectItems/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import {
   TouchableWithoutFeedback,
   ActivityIndicator,
@@ -198,9 +197,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-SelectItems.propTypes = {
-  statusBarColor: PropTypes.string.isRequired,
-};
 
 export default SelectItems;
