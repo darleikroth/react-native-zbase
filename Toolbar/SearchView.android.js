6e1c63cd74f3e94d6022279ec92f40895e9779d0
Darlei Kroth <darlei@zettabrasil.com.br>
2020-08-11 10:13:39 -0300

    feat(android): improvemts at searchbar component (react-navigation v5 major compatibility)

--- Toolbar/SearchView.android.js
+++ Toolbar/SearchView.android.js
@@ -30,6 +30,11 @@ type Props = {
    */
   underlayColor?: string;
   /**
+   * If there is no `headerLeft` on toolbar, set this property to `false`.
+   * Used to calculate dimensions. Default value is `true`.
+   */
+  hasHeaderLeft?: boolean;
+  /**
    * If there is no `headerRight` on toolbar, set this property to `false`.
    * Used to calculate dimensions. Default value is `true`.
    */
@@ -51,16 +56,13 @@ type State = {
 };
 
 const screen = Dimensions.get('window');
-const searchWidth = screen.width - 64;
 
-class Search extends React.Component
-{
+class Search extends React.Component {
   props: Props;
   state: State;
   timeoutID: ?number = null;
 
-  constructor()
-  {
+  constructor() {
     super();
     this.onChangeText = this.onChangeText.bind(this);
     this.state = {
@@ -69,8 +71,7 @@ class Search extends React.Component
     };
   }
 
-  toggleSearchable()
-  {
+  toggleSearchable() {
     requestAnimationFrame(() => this.setState({
       isSearchable: !this.state.isSearchable,
       text: '',
@@ -78,30 +79,27 @@ class Search extends React.Component
     this.props.onChangeText && setTimeout(() => this.props.onChangeText(''), 200);
   }
 
-  onChangeText(text)
-  {
+  onChangeText(text) {
     this.setState({ text })
     clearTimeout(this.timeoutID);
     this.timeoutID = setTimeout(() => {
       this.props.onChangeText && this.props.onChangeText(text.toUpperCase());
-    }, 650);
+    }, 500);
   }
 
-  render()
-  {
-    const hasHeaderRight = this.props.hasHeaderRight,
-    searchWid = hasHeaderRight ? searchWidth : searchWidth + 56;
+  render() {
+    const style = this.props.hasHeaderRight ? { marginRight: 48 } : undefined;
+
     return (
-      <View style={[styles.container, {width: searchWid}]} >
+      <View style={[styles.container, style]} >
         {this.renderTitle()}
         {this.renderSearchButton()}
       </View>
     );
   }
 
-  renderTitle()
-  {
-    const {title, tintColor, titleStyle, hasHeaderRight} = this.props;
+  renderTitle() {
+    const {title, tintColor, titleStyle, hasHeaderRight, hasHeaderLeft} = this.props;
 
     if (!this.state.isSearchable) {
       return (
@@ -111,7 +109,15 @@ class Search extends React.Component
       );
     }
 
-    const inputWid = hasHeaderRight ? screen.width - 110 : screen.width - 80;
+    let inputWid = screen.width - 80;
+
+    if (hasHeaderLeft && hasHeaderRight) {
+      inputWid = screen.width - (124 + 48);
+    } else if (hasHeaderLeft) {
+      inputWid = screen.width - 124;
+    } else if (hasHeaderRight) {
+      inputWid = screen.width - 110;
+    }
 
     return (
       <TextInput
@@ -130,8 +136,7 @@ class Search extends React.Component
     );
   }
 
-  renderSearchButton()
-  {
+  renderSearchButton() {
     const search = this.state.isSearchable,
     iconColor = this.props.iconColor;
 
@@ -150,26 +155,26 @@ Search.defaultProps = {
   title: 'Título',
   tintColor: 'white',
   iconColor: 'white',
-  hasHeaderRight: true,
+  hasHeaderLeft: false,
+  hasHeaderRight: false,
 };
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: 'center',
+    borderRadius: 5,
+    flex: 1,
     flexDirection: 'row',
     height: 48,
-    borderRadius: 5,
-    alignItems: 'center',
   },
   title: {
     fontSize: 19,
     fontWeight: '500',
     textAlign: 'left',
-    marginHorizontal: 16,
   },
   input: {
     height: 48,
     fontSize: 16,
-    marginLeft: 16,
   },
 });
 
